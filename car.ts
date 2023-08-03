a72386e0832725cb58fb430ed65e65db81e9703f
Marco <mornsophannamis@gmail.com>
2023-08-03 15:03:37 +0700

    Apply generic to class car

--- car.ts
+++ car.ts
@@ -1,21 +1,54 @@
+//Decorator
+function logger(target: any) {
+    console.log(`Creating an instance of ${target.name}`);
+}
+
+function logMethod(target: any, key: string, descriptor: PropertyDecorator) {
+    console.log(`Calling ${key} method`);
+}
+
+function logProperty(target: any, key: string) {
+    console.log(`Accessing ${key} property`);
+}
+//End decorator
+
+//Generic
+function identity<T>(arg: T): T {
+    return arg;
+}
+
 interface Engine {
+    wheel: number;
+
     start(): void;
 }
 
+@logger
 class GasEngine implements Engine {
+    constructor() {
+
+    }
+
+    @logProperty
+    wheel: number;
+
     start() {
-        console.log('Start the engine using gasoline');
+        console.log('Start the engine using gasoline.', identity<number>(12345));
     }
 }
 
 class ElectricEngine implements Engine {
+    wheel: number;
+    
     start(): void {
-        console.log('Start the engine using electricity');
+        console.log('Start the engine using electricity.');
     }
 
 }
 
 class WaterEngine implements Engine {
+    wheel: number;
+
     start(): void {
         console.log('Start the engine using water');
     }
@@ -26,8 +59,8 @@ class DependencyInjectionContainer {
     private dependencies: Map<string, Function> = new Map();
 
     constructor() {
-        this.dependencies.set('gas', () => new GasEngine());
-        this.dependencies.set('electric', () => new ElectricEngine());
+        this.dependencies.set(GasEngine.name, () => new GasEngine());
+        this.dependencies.set(ElectricEngine.name, () => new ElectricEngine());
     }
 
     createDependency(type: string) {
@@ -60,5 +93,5 @@ class Car {
 }
 
 const container = new DependencyInjectionContainer();
-const bmw: Car = new Car(container.createDependency('gas'));
+const bmw: Car = new Car(container.createDependency(GasEngine.name));
 bmw.startEngine();
